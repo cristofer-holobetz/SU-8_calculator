eb380f2b08a666192fd128dbaaa6a381c88c2c17
Cristofer Holobetz <cristofer.holobetz@berkeley.edu>
2018-10-26 23:44:19 -0700

    It works! Next is to make sure that out-of-range heights for each type of photoresist are excluded, otherwise you get weird things like negative spin speeds and stuff.

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,4 @@
 import csv
-import itertools
 
 
 def print_values(channel_height, resist_type):
@@ -8,16 +7,21 @@ def print_values(channel_height, resist_type):
     spin_speed = calc_spin_speeds(channel_height, resist_type)
     sb_times = calc_sb_times(channel_height, resist_type)
     exp_energy = calc_exp_energy(channel_height, resist_type)
-    peb_time = calc_peb_time(channel_height, resist_type)
+    peb_times = calc_peb_time(channel_height, resist_type)
     dev_time = calc_dev_time(channel_height, resist_type)
-    print('Spin coat at v1 = 300 rpm, a1 = 100 rpm/s and v2 = ' + spin_speed + ', a2 = 300 rpm/s')
+
+    print('Spin coat at v1 = 300 rpm, a1 = 100 rpm/s and v2 = ' + str(spin_speed) + ', a2 = 300 rpm/s.')
     if len(sb_times) == 2:
-        print('Perform Soft Bake for ' + sb_times[0] + ' minutes at 65C and ' + sb_times[1] + ' minutes at 95C.')
+        print('Perform Soft Bake for ' + str(sb_times[0]) + ' minutes at 65C and ' + str(sb_times[1]) + ' minutes at 95C.')
     else:
-        print('Perform Soft Bake for ' + sb_times + ' minutes.')
-    print('Expose surface to ' + exp_energy + ' mJ/cm^2')
-    print('Perform Post Exposure Bake for ' + peb_time + ' minutes')
-    print('Develop wafer for ' + dev_time + ' minutes')
+        print('Perform Soft Bake for ' + str(sb_times) + ' minutes.')
+    print('Expose surface to ' + str(exp_energy) + ' mJ/cm^2.')
+    if len(peb_times) == 2:
+        print('Perform Post Exposure Bake for ' + str(peb_times[0]) + ' minutes at 65C and '
+              + str(peb_times[1]) + ' minutes at 95C.')
+    else:
+        print('Perform Post Exposure Bake for ' + str(peb_times) + ' minutes.')
+    print('Develop wafer in SU-8 Developer for ' + str(dev_time) + ' minutes.')
 
 
 def choose_resist_range(su8_type):
@@ -35,16 +39,6 @@ def choose_resist_range(su8_type):
         print('This photoresist is either unsupported or does not exist')
         return
 
-# Using HOFs to return a suitable linear function for any input range
-
-
-def make_linear_func(height, converted_curve):
-    flanking_point_finder = find_flanking_points_maker(height, is_upward_slope(converted_curve))
-    point_one = flanking_point_finder(converted_curve)[0]
-    point_two = flanking_point_finder(converted_curve)[1]
-
-    return linear_func(point_one[0], point_one[1],  point_two[0], point_two[1])
-
 # Checks to see whether or not this curve slopes upwards or downwards. This could cause problems
 # if non-monotonic data sets are added in the future.
 
@@ -54,7 +48,7 @@ def is_upward_slope(converted_curve):
     y_one = float(converted_curve[0][1])
     y_two = float(converted_curve[1][1])
 
-    return y_one > y_two
+    return y_two > y_one
 
 # Uses a HOF to produce the correct procedure for finding relevant section of the curve
 # TODO: Add error-catching to catch people who specify a height outside the range
@@ -70,15 +64,31 @@ def find_flanking_points_maker(height, upward_slope):
         else:
             while float(converted_curve[row][0]) > height:
                 row += 1
-
-        x1 = float(converted_curve[row][0])
-        y1 = float(converted_curve[row][1])
-        x2 = float(converted_curve[row + 1][0])
-        y2 = float(converted_curve[row + 1][1])
+        # For the case where the last value in the set is your desired height
+        if row == len(converted_curve) - 1 and converted_curve[row] == height:
+            x1 = float(converted_curve[row - 1][0])
+            y1 = float(converted_curve[row - 1][1])
+            x2 = float(converted_curve[row][0])
+            y2 = float(converted_curve[row][1])
+        else:
+            x1 = float(converted_curve[row][0])
+            y1 = float(converted_curve[row][1])
+            x2 = float(converted_curve[row + 1][0])
+            y2 = float(converted_curve[row + 1][1])
 
         return (x1, y1), (x2, y2)
 
     return find_flanking_points
+
+# Using HOFs to return a suitable linear function for any input range
+
+
+def make_linear_func(height, converted_curve):
+    flanking_point_finder = find_flanking_points_maker(height, is_upward_slope(converted_curve))
+    point_one = flanking_point_finder(converted_curve)[0]
+    point_two = flanking_point_finder(converted_curve)[1]
+
+    return linear_func(point_one[0], point_one[1],  point_two[0], point_two[1])
 
 
 def linear_func(x1, y1, x2, y2):
@@ -110,15 +120,15 @@ def calc_sb_times(height, su8_type):
 
     with open('assets/sb_times/SB_SU-8_' + curve_range + '.csv', mode='r') as csvfile:
 
-        reader1, reader2 = itertools.tee(csv.reader(csvfile, delimiter=','))
-        num_cols = len(next(reader1))
-        del reader1
+        reader = csv.reader(csvfile, delimiter=',')
+        sb_both_temps = [tuple([col for col in row]) for row in reader]
 
-        sb_65_times = [(float(row[0]), float(row[1])) for row in reader2]
+        num_cols = len(sb_both_temps[0])
+        sb_65_times = [(row[0], row[1]) for row in sb_both_temps]
         sb_65_func = make_linear_func(height, sb_65_times)
 
         if num_cols == 3:
-            sb_95_times = [(float(row[0]), float(row[2])) for row in reader2]
+            sb_95_times = [(row[0], row[2]) for row in sb_both_temps]
             sb_95_func = make_linear_func(height, sb_95_times)
             return sb_65_func(height), sb_95_func(height)
 
@@ -140,16 +150,16 @@ def calc_peb_time(height, su8_type):
     curve_range = choose_resist_range(su8_type)
 
     with open('assets/peb_times/PEB_SU-8_' + curve_range + '.csv', mode='r') as csvfile:
+        #  fix the bit I was stuck on in calc_sb_time
+        reader = csv.reader(csvfile, delimiter=',')
+        peb_both_temps = [tuple([col for col in row]) for row in reader]
+        num_cols = len(peb_both_temps[0])
 
-        reader1, reader2 = itertools.tee(csv.reader(csvfile, delimiter=','))
-        num_cols = len(next(reader1))
-        del reader1
-
-        peb_65_times = [(float(row[0]), float(row[1])) for row in reader2]
+        peb_65_times = [(row[0], row[1]) for row in peb_both_temps]
         peb_65_func = make_linear_func(height, peb_65_times)
 
         if num_cols == 3:
-            peb_95_times = [(float(row[0]), float(row[2])) for row in reader2]
+            peb_95_times = [(row[0], row[2]) for row in peb_both_temps]
             peb_95_func = make_linear_func(height, peb_95_times)
             return peb_65_func(height), peb_95_func(height)
 
@@ -163,7 +173,7 @@ def calc_dev_time(height, su8_type):
         reader = csv.reader(csvfile, delimiter=',')
         develop_times = [tuple([float(val) for val in row]) for row in reader]
 
-        develop_func = make_linear_func(height, [develop_times])
+        develop_func = make_linear_func(height, develop_times)
         return develop_func(height)
 
 
