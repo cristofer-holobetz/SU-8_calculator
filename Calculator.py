2d35f34930d9963fce446e6fd8b76190df3d39fb
Cristofer Holobetz <cristofer.holobetz@berkeley.edu>
2018-10-26 21:43:25 -0700

    I think that I need to change calc_peb_time to reflect the possibility of two different PEB times, similar to the calc_sb_time. Other than that, finding the relevant values that flank the desired height has been changed so that it can be done in two cases. The first is when the curve monotonically increases, and the second is when it decreases. This version of the functionality is not robust to mon-monotonic curves that could be added in the future, and it's also not robust to curves that are constant between x1 and x2 (due to the way curve direction is calculated).

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,10 @@
 import csv
+import itertools
 
 
 def print_values(channel_height, resist_type):
+    channel_height = float(channel_height)
+    resist_type = float(resist_type)
     spin_speed = calc_spin_speeds(channel_height, resist_type)
     sb_times = calc_sb_times(channel_height, resist_type)
     exp_energy = calc_exp_energy(channel_height, resist_type)
@@ -36,15 +39,46 @@ def choose_resist_range(su8_type):
 
 
 def make_linear_func(height, converted_curve):
-    row, col = 0, 0
-    while converted_curve[row][0] < height:
-        row += 1
-    x1 = float(converted_curve[row][0])
-    y1 = float(converted_curve[row][1])
-    x2 = float(converted_curve[row + 1][0])
-    y2 = float(converted_curve[row + 1][1])
+    flanking_point_finder = find_flanking_points_maker(height, is_upward_slope(converted_curve))
+    point_one = flanking_point_finder(converted_curve)[0]
+    point_two = flanking_point_finder(converted_curve)[1]
 
-    return linear_func(x1, y1, x2, y2)
+    return linear_func(point_one[0], point_one[1],  point_two[0], point_two[1])
+
+# Checks to see whether or not this curve slopes upwards or downwards. This could cause problems
+# if non-monotonic data sets are added in the future.
+
+
+def is_upward_slope(converted_curve):
+
+    y_one = float(converted_curve[0][1])
+    y_two = float(converted_curve[1][1])
+
+    return y_one > y_two
+
+# Uses a HOF to produce the correct procedure for finding relevant section of the curve
+# TODO: Add error-catching to catch people who specify a height outside the range
+
+
+def find_flanking_points_maker(height, upward_slope):
+
+    def find_flanking_points(converted_curve):
+        row, col = 0, 0
+        if upward_slope:
+            while float(converted_curve[row][0]) < height:
+                row += 1
+        else:
+            while float(converted_curve[row][0] > height):
+                row += 1
+
+        x1 = float(converted_curve[row][0])
+        y1 = float(converted_curve[row][1])
+        x2 = float(converted_curve[row + 1][0])
+        y2 = float(converted_curve[row + 1][1])
+
+        return (x1, y1), (x2, y2)
+
+    return find_flanking_points
 
 
 def linear_func(x1, y1, x2, y2):
@@ -73,12 +107,16 @@ def calc_sb_times(height, su8_type):
     curve_range = choose_resist_range(su8_type)
 
     with open('assets/sb_times/SB_SU-8_' + curve_range + '.csv', mode='r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        sb_65_times = [(row[0], row[1]) for row in reader]
+
+        reader1, reader2 = itertools.tee(csv.reader(csvfile, delimiter=','))
+        num_cols = len(next(reader1))
+        del reader1
+
+        sb_65_times = [(float(row[0]), float(row[1])) for row in reader2]
         sb_65_func = make_linear_func(height, sb_65_times)
 
-        if len(reader[0]) == 3:
-            sb_95_times = [(row[0], row[2]) for row in reader]
+        if num_cols == 3:
+            sb_95_times = [(float(row[0]), float(row[2])) for row in reader2]
             sb_95_func = make_linear_func(height, sb_95_times)
             return sb_65_func(height), sb_95_func(height)
 
@@ -90,7 +128,7 @@ def calc_exp_energy(height, su8_type):
 
     with open('assets/exposure_energies/EE_SU-8_' + curve_range + '.csv', mode='r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        exp_energies = [tuple([val for val in row]) for row in reader]
+        exp_energies = [tuple([float(val) for val in row]) for row in reader]
 
         ee_func = make_linear_func(height, exp_energies)
         return ee_func(height)
@@ -101,7 +139,7 @@ def calc_peb_time(height, su8_type):
 
     with open('assets/peb_times/PEB_SU-8_' + curve_range + '.csv', mode='r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        peb_times = [tuple([val for val in row]) for row in reader]
+        peb_times = [tuple([float(val)for val in row]) for row in reader]
 
         peb_func = make_linear_func(height, [peb_times])
         return peb_func(height)
@@ -112,14 +150,14 @@ def calc_dev_time(height, su8_type):
 
     with open('assets/development_times/DT_SU-8_' + curve_range + '.csv', mode='r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        develop_times = [tuple([val for val in row]) for row in reader]
+        develop_times = [tuple([float(val) for val in row]) for row in reader]
 
         develop_func = make_linear_func(height, [develop_times])
         return develop_func(height)
 
 
 user_su8_type = input('Select SU-8 Type:')
-user_height = input('Select desired channel height in µm:')
+user_height = float(input('Select desired channel height in µm:'))
 
 # Reads type of SU-8 user wants to use and selects correct files
 
